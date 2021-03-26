ff101680a3eb2b9ff75a1496891a8fe083084879
Yash-y-ash <wardhan696@gmail.com>
2021-03-27 00:25:14 +0530

    Added:adding multiple person details

--- src/com/addressbooksystem/AddressBookMain.java
+++ src/com/addressbooksystem/AddressBookMain.java
@@ -8,9 +8,36 @@ import java.util.*;
 
 public class AddressBookMain {
 
-	public static void main(String[] args) {
-		System.out.println("person details");
-		Person person = new Person("yash", "wardhan", "banglore", "karnataka" , "btm 2nd stage", "wardh@gmail.com", 45637, 88252245);
+	public void addNewPerson() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("First Name");
+		String firstName = scanner.nextLine();
+		System.out.println("Last Name");
+		String lastName = scanner.nextLine();
+		System.out.println("address");
+		String address = scanner.nextLine();
+		System.out.println("State");
+		String state = scanner.nextLine();
+		System.out.println("City");
+		String city = scanner.nextLine();
+		System.out.println("Zip");
+		int zipCode = scanner.nextInt();
+		System.out.println("Phone Number");
+		long phoneNumber = scanner.nextLong();
+		System.out.println("Email");
+		String email = scanner.next();
+
+		Person person = new Person( firstName, lastName , city,  state,  address,  email,  zipCode, phoneNumber);
 		person.showPersonsDetails();
+		System.out.println("new person details added");
+	}
+
+
+	public static void main(String args[]) {
+
+		AddressBookMain addressBook = new AddressBookMain();
+		addressBook.addNewPerson();
+
 	}
 }
+
